f0981cf77ad8d16e501b1938108bce00f0dd76a7
Sirui Tan <Sirui Tan@utoronto.ca>
2024-03-08 17:21:41 -0500

    add test_data

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -10,6 +10,33 @@
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+library(testthat)
 
 #### Test data ####
+cces2022 <-
+  read_csv(file = here::here("data/analysis_data/cces2022_clean.csv"))
+
+test_that("Number of observations is correct", {
+  expect_true(nrow(cces2022) > 0)
+})
+
+test_that("All records have valid values for voted_for", {
+  expect_true(all(cces2022$voted_for %in% c("Dem", "Rep")))
+})
+
+test_that("All records have valid values for gender", {
+  expect_true(all(cces2022$gender %in% c("Man", "Woman", "Non-binary", "Other")))
+})
+
+
+test_that("All records have valid values for education", {
+  expect_true(all(cces2022$education %in% c("No HS", "High school graduate", "Some college", "2-year", "4-year", "Post-grad")))
+})
+
+test_that("All records have valid values for race", {
+  expect_true(all(cces2022$race %in% c("White", "Black", "Hispanic", "Asian", "Native American", "Middle Eastern", "Two or more races", "Other")))
+})
+
+test_that("No missing values in the dataset", {
+  expect_false(any(is.na(cces2022)))
+})
